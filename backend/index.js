a6b7c18231f110e76ce1a0848cc6ac539381be5b
lopesdaluz <tlopesdaluz@gmail.com>
2023-11-02 18:32:40 +0100

    use PostgreSQL

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 // nodemon
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
 
 const express = require("express"),
   path = require("path");
 
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
 const app = express();
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "WORLD" });
+app.get("/api", async (_request, response) => {
+  const { rows } = await client.query("SELECT * FROM lopes ");
+
+  response.send(rows);
 });
 
 app.use(express.static(path.join(path.resolve(), "public")));
